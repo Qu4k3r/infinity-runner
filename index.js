3995e4589cf8f7a433270e89d4eaf3d6d348a508
Qu4k3r <neves0007@gmail.com>
2020-10-05 21:30:01 -0300

    Remove node auto-generated files and update .gitignore

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 'use strict'
 
+// const debug = require('debug')('nodestr:server')
+// const express = require('express')
+// const app = express()
+// app.set('port', port)
+
 const http = require('http')
-const debug = require('debug')('nodestr:server')
-const express = require('express')
-
-const app = express()
+const hostname = '127.0.0.1'
 const port = 3000
-app.set('port', port)
-
-const server = http.createServer(app)
-const router = express.Router()
-
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "Node Store API",
-        version: "0.0.1"
-    })
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('Hello there!\n')
 })
-app.use('/', route)
 
-server.listen(port)
-console.log('API rodando na porta ' + port)
+// const router = express.Router()
+
+// app.use('/', route)
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`)
+})
